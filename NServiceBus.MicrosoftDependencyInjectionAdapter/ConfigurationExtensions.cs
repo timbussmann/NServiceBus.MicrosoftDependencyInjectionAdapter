f678636f8904966da5458d3a94b4a0f863c068f6
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-03-14 20:42:19 +0100

    add configuration extension methods

---
+++ NServiceBus.MicrosoftDependencyInjectionAdapter/ConfigurationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus.Container;
+
+namespace NServiceBus.MicrosoftDependencyInjectionAdapter
+{
+    public static class ConfigurationExtensions
+    {
+        private const string FactoryKey = "MSDIAdapter.Factory";
+
+        public static ContainerCustomizations UseServiceCollection(this ContainerCustomizations customizations, IServiceCollection serviceCollection)
+        {
+            customizations.Settings.Set<IServiceCollection>(serviceCollection);
+            return customizations;
+        }
+
+        public static ContainerCustomizations ServiceProviderFactory(this ContainerCustomizations customizations,
+            Func<IServiceCollection, IServiceProvider> providerFactory)
+        {
+            customizations.Settings.Set(FactoryKey, providerFactory);
+            return customizations;
+        }
+    }
+}
