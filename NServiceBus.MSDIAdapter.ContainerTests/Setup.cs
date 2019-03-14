53bda26b61821abf7e4ae617bdef0556abc8db27
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-03-14 20:43:55 +0100

    fix test setup

--- NServiceBus.MSDIAdapter.ContainerTests/Setup.cs
+++ NServiceBus.MSDIAdapter.ContainerTests/Setup.cs
@@ -3,14 +3,11 @@ using NServiceBus.ContainerTests;
 using NServiceBus.MicrosoftDependencyInjectionAdapter;
 using NUnit.Framework;
 
-namespace NServiceBus.MSDIAdapter.ContainerTests
+[SetUpFixture]
+public class Setup
 {
-    [SetUpFixture]
-    public class Setup
+    public Setup()
     {
-        public Setup()
-        {
-            TestContainerBuilder.ConstructBuilder = () => new ConfigurableServiceCollectionAdapter(new ServiceCollection(), sc => sc.BuildServiceProvider());
-        }
+        TestContainerBuilder.ConstructBuilder = () => new ConfigurableServiceCollectionAdapter(new ServiceCollection(), sc => sc.BuildServiceProvider());
     }
 }
